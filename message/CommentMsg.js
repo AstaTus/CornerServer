ec58e5ff5eec9170eff3f57de2d89ae9f1143e8d
AstaTus <chenjunqi5@126.com>
2016-03-02 22:26:20 +0800

    article comment up

---
+++ message/CommentMsg.js
@@ -0,0 +1,16 @@
+function CommentMsg(){
+    this.mArticleGuid;
+    this.mGuids = new Array();
+    this.mReplyGuids = new Array();
+    this.mReplyNames = new Array();
+    this.mHeadUrls = new Array();
+
+    this.mTargetGuids = new Array();
+    this.mTargetNames = new Array();
+    this.mTimes = new Array();
+    this.mTexts = new Array();
+
+}
+
+
+module.exports = CommentMsg
